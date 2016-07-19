4df71f37cad19da532982616c67426fbfae020af
woolseyyy <woolsey@zju.edu.cn>
2016-07-19 17:29:06 +0800

    注册功能

--- model/user.js
+++ model/user.js
@@ -15,7 +15,8 @@ module.exports = mongoose.model('user', new Schema({
     },
     wechatID:{
         type: String,
-        unique : true//为了支持第三方登陆，要求微信号唯一绑定
+        unique : true,//为了支持第三方登陆，要求微信号唯一绑定
+        sparse : true//稀疏索引，允许该字段虽然唯一但允许为空
     },
     baseinfo: {
         //2.0版本再补充
@@ -23,33 +24,4 @@ module.exports = mongoose.model('user', new Schema({
     tableID: [String]
 }));
 
-/*
-User.login = function(user, callback){
 
-    if(!user.name || !user.password)//检查用户名和密码是否为空
-    {
-        return;
-    }
-    //检查用户名是否为空
-    User.findOne({username:user.name},
-        function(err,doc){
-            if(err){
-                return callback(0, err);//0表示登陆失败
-            }
-            else{
-                if(!doc){
-                    return callback(0, '用户名不存在！');
-                }
-                else{
-                    if(doc.password == user.password){
-                        return callback(1, "登陆成功！");
-                    }
-                    else{
-                        return callback(0, "用户名或密码错误！");
-                    }
-                }
-            }
-        }
-    );
-}
-*/
--- routes/index.js
+++ routes/index.js
@@ -43,6 +43,74 @@ module.exports = require('express').Router()
                 }
             }
         })
-    });
+    })
+    .post('/signup', function(req, res, next){
+        var username = req.body.username;
+        var password = req.body.password;
+
+        if(!username || !password) {
+            res.json({
+                code: -1,
+                msg: '有参数为空',
+                body:{}
+            });
+        }
+        else{
+            var test = /^\d{11,}$/;
+            if(!(test.test(username))){
+                res.json({
+                    code: -2,
+                    msg: '用户名格式错误',
+                    body:{}
+                });
+            }
+            else{
+                User.findOne({
+                    username : username
+                },function(err,doc){
+                    if(err){
+                        console.log(err);
+                        res.json({
+                            code: -3,
+                            msg: '检验用户名时数据库错误'+err,
+                            body: {}
+                        });
+                    }
+                    else{
+                        if(doc){
+                            res.json({
+                                code: -4,
+                                msg: '用户名已存在',
+                                body: {}
+                            });
+                        }
+                        else{
+                            new User({
+                                username: username,
+                                password: password
+                            }).save(function(err, user){
+                                if(err){
+                                    console.log(err);
+                                    res.json({
+                                        code: -3,
+                                        msg: '数据库错误'+err,
+                                        body: {}
+                                    });
+                                }
+                                else{
+                                    req.session.user = user;//存入会话
+                                    res.json({
+                                        code: 0,
+                                        msg: '注册成功',
+                                        body: {}
+                                    });
+                                }
+                            });
+                        }
+                    }
+                })
+            }
+        }
+    })
 
 

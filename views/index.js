f706f494525fd9b49dadc03e0655e2174a830b93
woolseyyy <woolsey@zju.edu.cn>
2016-07-24 15:39:20 +0800

    路由修复

--- views/index.js
+++ views/index.js
@@ -8,14 +8,14 @@ var Router = require('react-router').Router;
 var Route = require('react-router').Route;
 var IndexRoute = require('react-router').IndexRoute;
 var Link = require('react-router').Link;
-var browserHistory = require('react-router').browserHistory;
+var hashHistory = require('react-router').hashHistory;
 
 var sign = require('./containers/signin.jsx');
 var signinBox = require('./containers/component/signinBox.jsx');
 var signupBox = require('./containers/component/signupBox.jsx');
 
 render((
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
         <Route path="/" component={sign}/>
         <Route path="/sign" component={sign}>
             <IndexRoute component={signupBox} />

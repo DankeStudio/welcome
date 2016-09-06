613ed2ae1d6ebef3ec620a7aa279ed74541d144b
woolseyyy <woolsey@zju.edu.cn>
2016-09-06 20:25:28 +0800

    跨域测试通过

--- models/support/grant.js
+++ models/support/grant.js
@@ -31,8 +31,11 @@ module.exports = {
     //跨域请求 cors
     cors: function(req, res, next){
         res.header("Access-Control-Allow-Origin", "114.215.144.43:80");
+        //res.header("Access-Control-Allow-Origin", "http://localhost");
+        //res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
         res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-        if(req.method=="OPTIONS") res.send(200);/*让options请求快速返回*/
-        else  next();
+        res.header("X-Powered-By",' 3.2.1');
+        next();
     }
 }
